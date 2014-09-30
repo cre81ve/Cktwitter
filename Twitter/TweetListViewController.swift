94fd69efb3c3540f4e635a39daa3c91961409993
Chaitanya <chaitanyakumar.k@gmail.com>
2014-09-30 00:18:47 -0700

    Twitter IOS - ck

---
+++ Twitter/TweetListViewController.swift
@@ -0,0 +1,208 @@
+//
+//  TweetListViewController.swift
+//  Twitter
+//
+//  Created by CK on 9/25/14.
+//  Copyright (c) 2014 Chaitanya Kannali. All rights reserved.
+//
+
+import UIKit
+
+class TweetListViewController: UIViewController ,UITableViewDataSource , UITableViewDelegate {
+    
+    @IBOutlet weak var retweetCnt: UILabel!
+    
+    @IBOutlet weak var favCnt: UILabel!
+    
+    @IBOutlet var replyGesture: UITapGestureRecognizer!
+    
+    @IBOutlet weak var reTweetGesture: UIImageView!
+    
+    @IBOutlet weak var favouriteGesture: UIImageView!
+    
+    @IBOutlet weak var tweetsTable: UITableView!
+    var tweets = [Tweet]()
+    var refreshControl:UIRefreshControl = UIRefreshControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetsTable.delegate = self
+        tweetsTable.dataSource = self
+        tweetsTable.rowHeight = UITableViewAutomaticDimension
+        
+        tweetsTable.hidden = true
+        refreshTweets()
+        
+        self.refreshControl = UIRefreshControl()
+        self.refreshControl.attributedTitle = NSAttributedString(string: "Pull to refersh")
+        self.refreshControl.addTarget(self, action: "refreshTweets", forControlEvents: UIControlEvents.ValueChanged)
+        self.tweetsTable.insertSubview(refreshControl, atIndex: 0)
+        Store.store("reply.name", val: "")
+        Store.store("reply.id" , val: "")
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidDisappear(true)
+        refreshTweets()
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+        Store.store("reply.name", val: "")
+        Store.store("reply.id" , val: "")
+        
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, estimatedHeightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+    
+    func refreshTweets() {
+        MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        Tweet.tweetsHomeTimeLineWithParams(nil, completion: { (tweets, error) -> () in
+            if(tweets != nil) {
+                self.tweets = tweets!
+                for tw in tweets! {
+                    var t = tw as Tweet
+                }
+                self.tweetsTable.reloadData()
+                self.refreshControl.endRefreshing()
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
+                self.tweetsTable.hidden = false
+            }
+            else {
+                NSLog("Error in loading tweets")
+                MBProgressHUD.hideHUDForView(self.view, animated: true)
+                self.refreshControl.endRefreshing()
+            }
+        })
+    }
+    
+    @IBAction func onLogout(sender: AnyObject) {
+        User.currentUser?.logout()
+        var tvc = self.storyboard?.instantiateViewControllerWithIdentifier("loginViewController") as UIViewController
+        self.presentViewController(tvc, animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
+    @IBAction func onReply(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func onFavourite(sender: AnyObject) {
+        
+        
+    }
+    
+    @IBAction func onRetweet(sender: AnyObject) {
+    }
+    
+    @IBOutlet var onFavourite: UITapGestureRecognizer!
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var tc:TweetCell = tableView.dequeueReusableCellWithIdentifier("tweetCellId") as TweetCell
+        if(tweets.count > 0 ) {
+            var tweet:Tweet = tweets[indexPath.row] as Tweet
+            var user:User =  tweet.user!
+            tc.tweetText.text = tweet.text
+            var profileImageUrl = user.profileImageUrl
+            var retweeted:Bool = false
+            var userscreenname = user.screenName
+            var userfullname = user.name
+            if((tweet.originalUser) != nil) {
+                retweeted = true
+            }
+            if(retweeted) {
+                profileImageUrl = tweet.originalUser?.profileImageUrl
+                userfullname = tweet.originalUser?.name
+                userscreenname = tweet.originalUser?.screenName
+                NSLog(" Retweeted post by \(userscreenname)")
+            }else {
+                NSLog(" Normal post by \(userscreenname)")
+                
+            }
+            tc.userProfileImage.setImageWithURL(NSURL(string: profileImageUrl!))
+            tc.userFullName.text = userfullname
+            tc.userName.text = userscreenname
+            tc.tweet = tweet
+            
+            if((tweet.user) != nil && retweeted ) {
+                tc.reTweetedByImage.setImageWithURL(NSURL(string:tweet.imageRetweeted))
+                tc.reTweetedByy.text = tweet.user?.name
+                showRetweeted(tc)
+            }else {
+               hideRetweeted(tc)
+                
+            }
+            tc.replyImage.setImageWithURL(NSURL(string:tweet.imageReply))
+            tc.reTweetActionImage.setImageWithURL(NSURL(string:tweet.imageRetweet))
+            tc.favouritesImage.setImageWithURL(NSURL(string:tweet.imageFavourite))
+            tc.retweetCnt.text = String(tweet.retweetCount!)
+            tc.favCnt.text = String(tweet.favouriteCount!)
+            if(tweet.favourited > 0) {
+                tc.favouritesImage.setImageWithURL(NSURL(string:tweet.imageFavouriteOn))
+            }
+            if(tweet.retweeted > 0) {
+                tc.reTweetActionImage.setImageWithURL(NSURL(string:tweet.imageRetweeted))
+            }
+            tc.timeSinceLabel.text = tweet.createdAt?.prettyTimestampSinceNow()
+        }
+        return tc
+    }
+    
+    
+    func hideRetweeted(tc: TweetCell) {
+        //CONSTRAINT STUFF
+        tc.selfRTConstraint.constant = 0
+        tc.selfRTLabelConstraint.constant = 0
+        tc.reTweetedByy.hidden = true
+        tc.reTweetedByImage.hidden = true
+    }
+    
+    func showRetweeted(tc: TweetCell) {
+        //CONSTRAINT STUFF
+        tc.selfRTConstraint.constant = 15
+        tc.selfRTLabelConstraint.constant = 14
+        tc.reTweetedByy.hidden = false
+        tc.reTweetedByImage.hidden = false
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        var tweetSelected = tweets[indexPath.row] as Tweet
+        Tweet.currentTweet = tweetSelected
+        var tdc = self.storyboard?.instantiateViewControllerWithIdentifier("tweetDetailController") as TweetDetailController
+        var segue = UIStoryboardSegue(identifier: "tweetDetailSegue", source: self, destination: tdc)
+        self.prepareForSegue(segue, sender: self)
+    }
+    
+    
+    
+    @IBAction func onShowCompose(sender: AnyObject) {
+        var tdc = self.storyboard?.instantiateViewControllerWithIdentifier("newTweetController") as NewTweetController
+        var segue = UIStoryboardSegue(identifier: "tweetComposeSegue", source: self, destination: tdc)
+        self.prepareForSegue(segue, sender: self)
+        
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
+    
+}
---
+++ Twitter/User.swift
@@ -0,0 +1,93 @@
+//
+//  User.swift
+//  Twitter
+//
+//  Created by CK on 9/27/14.
+//  Copyright (c) 2014 Chaitanya Kannali. All rights reserved.
+//
+
+import UIKit
+
+var _currentUser: User?
+let currentUserKey = "kCurrentUserKey"
+let userDidLoginNotification = "userDidLoginNotification"
+let userDidLogoutNotification = "userDidLogoutNotification"
+
+class User: NSObject {
+    
+    var name: String?
+    var screenName: String?
+    var profileImageUrl: String?
+    var tagline: String?
+    var dictionary: NSDictionary
+    
+    // user initialization with JSON Dictionary
+    
+    init(dictionary: NSDictionary) {
+        self.dictionary = dictionary
+        self.screenName = dictionary["screen_name"] as? String
+        self.name = dictionary["name"] as? String
+        self.profileImageUrl = dictionary["profile_image_url"] as? String
+        self.tagline = dictionary["description"] as? String
+        
+    }
+    
+    
+    // Logout function for user
+    
+    func logout() {
+        User.currentUser = nil
+        TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
+        NSNotificationCenter.defaultCenter().postNotificationName(userDidLogoutNotification, object: nil)
+    }
+    
+    
+    // Lgin function notification
+    
+    func login() -> Bool{
+        var success:Bool = false
+        TwitterClient.sharedInstance.loginWithCompletion() {
+            (user: User? ,error:NSError?) in
+            
+            if user != nil {
+                NSNotificationCenter.defaultCenter().postNotificationName(userDidLoginNotification, object: nil)
+                success = true
+            }
+            else {
+                //handle login error
+                success = false
+            }
+        }
+        
+        return success
+        
+    }
+    
+    class var currentUser: User? {
+        
+        get {
+            if _currentUser == nil {
+                var data = NSUserDefaults.standardUserDefaults().objectForKey(currentUserKey) as? NSData
+                if(data != nil) {
+                    var dictionary = NSJSONSerialization.JSONObjectWithData(data!, options: nil, error: nil) as NSDictionary
+                    _currentUser = User(dictionary: dictionary)
+                }
+            }
+            return _currentUser
+        }
+                
+        set(user) {
+            _currentUser = user
+            if(_currentUser != nil) {
+                var data = NSJSONSerialization.dataWithJSONObject(user!.dictionary, options: nil, error: nil)
+                NSUserDefaults.standardUserDefaults().setObject(data, forKey: currentUserKey)
+            }else {
+                NSUserDefaults.standardUserDefaults().setObject(nil, forKey: currentUserKey)
+            }
+            NSUserDefaults.standardUserDefaults().synchronize()
+            
+        }
+        
+    }
+    
+}
